881ad73001d576f948696c9d86ddcfcb378e3ebc
lfepy <lfepython@gmail.com>
2024-08-13 19:33:07 +0300

    Delete build/lib/lfepy directory

--- build/lib/lfepy/Helper/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py in the main package
-__version__ = '1.0.0'
-__author__ = ["Prof. Dr. Khalid M. Hosny", "B.Sc. Mahmoud A. Mohamed"]
-__license__ = 'MIT'
-
-# Example of a package-wide configuration
-DEBUG = False
